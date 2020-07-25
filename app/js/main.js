c8b6a9c4f59c6a0ca9e7f9e33d97bd5d915c0943
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-07-25 17:48:35 +0300

    added gallery + help +slider +footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,10 @@
 $(function () {
-  
+
+    $(".news__slider").slick({
+      prevArrow:
+        '<button type="button" class="slick-btn slick-prev">',
+      nextArrow:
+        '<button type="button" class="slick-btn slick-next">',
+    });
+
 });
