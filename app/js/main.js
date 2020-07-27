4629c1bd3a9ebb3fcdd7a6027cf4ae8cef311d1e
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2020-07-27 21:03:56 +0300

    mobile adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,11 +5,23 @@ $(function () {
         '<button type="button" class="slick-btn slick-prev">',
       nextArrow:
         '<button type="button" class="slick-btn slick-next">',
+        responsive: [
+          {
+            breakpoint: 1210,
+            settings: {
+              arrows:false
+            }
+          }
+        ]
     });
+
     $('.menu__burger').on('click', function (){
       $('.header__top-menu,.menu__burger').toggleClass('active');
       $('body').toggleClass('lock');
 
     });
+
+    
+
 
 });
